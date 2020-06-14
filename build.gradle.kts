a9184315ccb3c56319ac1d4f5c7ee91e0af35751
Nicolas <niggelgame@gmail.com>
2020-06-14 21:03:26 +0200

    Added Bintray-Uploading
    - Added Bintray Uploading Config and Build Possibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     `java-library`
      maven
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 repositories {
     jcenter()
 }
+
+group = "com.i18next"
+version = "1.0"
 
 dependencies {
     implementation("org.json:json:20200518")
@@ -15,5 +20,50 @@ dependencies {
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
 }
+val javaComponent = components["java"]
+
+tasks {
+    val sourcesJar = task<Jar>("sourcesJar") {
+        dependsOn(classes)
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    val javadocJar = task<Jar>("javadocJar") {
+        from(javadoc)
+        archiveClassifier.set("javadoc")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(javaComponent)
+                artifact(sourcesJar)
+                artifact(javadocJar)
+            }
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    setPublications("mavenJava")
+    pkg {
+        repo = "maven"
+        name = "i18next"
+        userOrg = "votebot"
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/votebot/i18next.git"
+        version {
+            name = project.version as String
+        }
+
+    }
+}
+
+fun com.jfrog.bintray.gradle.BintrayExtension.pkg(block: com.jfrog.bintray.gradle.BintrayExtension.PackageConfig.() -> Unit) = pkg(delegateClosureOf(block))
+fun com.jfrog.bintray.gradle.BintrayExtension.PackageConfig.version(block: com.jfrog.bintray.gradle.BintrayExtension.VersionConfig.() -> Unit) = version(delegateClosureOf(block))
+fun com.jfrog.bintray.gradle.BintrayExtension.VersionConfig.gpg(block: com.jfrog.bintray.gradle.BintrayExtension.GpgConfig.() -> Unit) = gpg(delegateClosureOf(block))
